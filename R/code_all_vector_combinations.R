a7bdb5cf895486119de8a4f2c57a24b11f6bd9e2
MattjCamp <mjcampbell74@gmail.com>
2016-09-12 06:50:14 -0400

    Added function that takes a vector and gives you all possible combinations of the items in the vector

---
+++ R/code_all_vector_combinations.R
@@ -0,0 +1,38 @@
+
+#' All Combinations of a List
+#'
+#' Returns all possible combinations of a list optionally appending some elements to make sure they are included in each list item
+#' @param vector the list of items
+#' @param include.these.in.each items that you want to make sure are included in each returned itme
+#' @keywords code automation
+#' @export
+#' @examples
+
+code_all_vector_combinations <- function(vector, include.these.in.each = NULL){
+
+  num.cat <- length(vector) - 1
+  c.all <- NULL
+  for (i in 1:num.cat) {
+    cats <- combn(vector, i, simplify = FALSE)
+    c.all <- c(c.all, cats)
+  }
+
+  num.c.all <- length(c.all)
+
+  for (i in 1:num.c.all) {
+    o <- order(c.all[[i]])
+    o <- unique(o)
+    c.all[[i]] <- c.all[[i]][o]
+  }
+
+ for (i in 1:num.c.all) {
+    gb <- c(include.these.in.each, c.all[[i]])
+    o <- order(gb)
+    c.all[[i]] <- unique(gb[o])
+  }
+
+  r <- unique(c.all)
+
+  r
+
+}
