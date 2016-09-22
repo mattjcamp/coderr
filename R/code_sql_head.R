180dd2121233b1263b549b3f76012add96de0183
mattjcamp <mjcampbell74@gmail.com>
2016-09-22 15:42:12 -0400

    Fixed sql_head so that it shows up with the other functions

--- R/code_sql_head.R
+++ R/code_sql_head.R
@@ -4,11 +4,12 @@
 #' SQL Code to get the top five records from a sample
 #' @keywords database, state, location
 #' @param sql an object of type {\link{code_sql_sample}}, character in SQL format or a {\link{db_table}} that has SQL which selects records
+#' @values character string SQL Statement
 #' @export
 #' @examples
 #
 
-sql_head <- function(sql){
+code_sql_head <- function(sql){
 
   if (!any(!c("sql_sample", "character", "db_table") %in% class(sql)))
     stop("sql_head requires sql to be one of these objects: character, sql_sample or db_table")
