93234068fdfc4385fc2a9981210bf42cead90af6
mattjcamp <mjcampbell74@gmail.com>
2016-11-28 10:34:53 -0500

    Added code_sql_summarize

---
+++ R/code_sql_summarize.R
@@ -0,0 +1,38 @@
+
+#' Summarize Records
+#'
+#' @description SQL Code to get descriptive statistics
+#' @details Summarizes the numeric columns from a SELECT statement or table
+#' @keywords stats
+#' @param sql an object of type {\link{code_sql_sample}}, character in SQL format that has SQL which selects records
+#' @param by summarize by these groups
+#' @export
+#' @examples
+#
+
+code_sql_summarize <- function(sql, cols, by = NULL,
+                               include_sdp = FALSE){
+
+  sql <- sql[1]
+
+  s <- "count(COL) as n_COL, avg(COL) as mean_COL%s, min(COL) as min_COL, max(COL) as max_COL"
+
+  if (include_sdp) {
+    s <- sprintf(s, ", stdevp(COL) as sdp_COL")
+  } else {
+    s <- sprintf(s, "")
+  }
+
+  s <- stringr::str_replace_all(s, "COL", cols)
+  s <- coderr::code_vector_to_csv_list(s, FALSE, FALSE)
+
+  if (is.null(by)) {
+    sql <- sprintf("SELECT %s FROM (%s) RECORDS", s, sql)
+  } else {
+    by <-  coderr::code_vector_to_csv_list(by, FALSE, FALSE)
+    sql <- sprintf("SELECT %s, %s FROM (%s) RECORDS GROUP BY %s", by, s, sql, by)
+  }
+
+  sql
+
+}
